2a8522e1533841504f4984b3afbdedc1ac567cb0
Andy Shevchenko <ur4ltz@gmail.com>
2015-10-27 02:29:18 +0200

    Add "magit Documentation".

--- config/25-git.el
+++ config/25-git.el
@@ -1,8 +1,14 @@
+
 (el-get 'sync '(magit
 		magit-filenotify
 		))
 
 (require 'magit)
+
+(with-eval-after-load 'info
+  (info-initialize)
+  (add-to-list 'Info-directory-list
+               "~/.emacs.d/package.d/magit/Documentation/"))
 
 (global-set-key (kbd "C-x g") 'magit-status)
 
